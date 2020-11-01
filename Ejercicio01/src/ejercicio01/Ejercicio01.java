2a9db99bcc6e98bdb1ea1bfe6b07e32cf8177f1e
Nixon Vuele <njvuele@utpl.edu.ec>
2020-10-31 19:29:48 -0500

    Trabajo v3.8

--- Ejercicio01/src/ejercicio01/Ejercicio01.java
+++ Ejercicio01/src/ejercicio01/Ejercicio01.java
@@ -10,26 +10,20 @@ package ejercicio01;
  * @author reroes
  */
 public class Ejercicio01 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // Resolver
-        
         // Crear una variable entera llamada edad
         // Asignar a la variable edad el valor de 20
-        
-        
+        int edad=20;
         // Crear una variable double llamada promedio
         // Asignar a la variable promedio el valor de 10.2
-        
-        
+        double promedio=10.2;
         // Crear una variable String llamada pais
         // Asignar a la variable pais el valor de Ecuador
-        
-        
-        
+        String pais="Ecuador";  
         System.out.println(edad);
         System.out.println(promedio);
         System.out.println(pais);
@@ -39,8 +33,6 @@ public class Ejercicio01 {
         
         double resultado = (1+2+4+3)/5+Math.sqrt(81);
         System.out.println(resultado);
-        
-        
     }
     
 }
---
+++ Ejercicio04/src/ejercicio04/Ejercicio04.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio04;
+
+/**
+ *
+ * @author Nixon
+ */
+public class Ejercicio04 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double resultado= 20/(1+3)+2;
+        System.out.println("Es "+(resultado >= 3)+" que "+resultado+" es >= 3");
+    }
+}
